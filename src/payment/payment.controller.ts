6ad8a1b60600671da23dd1c0c44f44da93c9f872
Bartek <26925320+Bagra001@users.noreply.github.com>
2024-01-08 21:53:15 +0100

    services

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CreditCardService } from './creditcard/creditcard.service';
+import { CurrentAccountService } from './current-account/current-account.service';
+import { PaypalService } from './paypal/paypal.service';
 
 @Controller('payment')
-export class PaymentController {}
+export class PaymentController {
+  constructor(
+    private paypalService: PaypalService,
+    private currentAccountService: CurrentAccountService,
+    private creditcardService: CreditCardService,
+  ) {}
+
+  @Get('/balance')
+  balance(): string {
+  return "your Balance is: 0,00 €";
+  }
+}
--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PaymentController } from './payment.controller';
+import { CreditCardService } from './creditcard/creditcard.service';
+import { CurrentAccountService } from './current-account/current-account.service';
+import { PaypalService } from './paypal/paypal.service';
 
 @Module({
   controllers: [PaymentController],
+  providers: [CreditCardService, CurrentAccountService, PaypalService],
 })
 export class PaymentModule {}
